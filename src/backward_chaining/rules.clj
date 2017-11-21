3e13ed02cb63c0949a281ce4f4d1bdf1d0c920d1
James Oldfield <hi@james-oldfield.co>
2017-11-21 15:41:50 +0000

    store working memory as set, and add operator functions

--- src/backward_chaining/rules.clj
+++ src/backward_chaining/rules.clj
@@ -1,26 +1,42 @@
 (ns backward-chaining.rules)
 
+; ----------------------
+; Modify initial working memory and rule base below:
+; ----------------------
+
 ; define seq to store the rulebase
+; :ante - list of symbols as predicates
+; :cons - list of symbols that follow from predicates being true
+; :operator - relationship between 
 (def base
   '({:numb 1
-     :ante [\f \h \a \c]
-     :cons [\b \g]}
+     :ante ("f h" "a c")
+     :cons ("b g")
+     :operator or}
     {:numb 2
-     :ante [\n \s]
-     :cons [\e \m]}
+     :ante ("n s")
+     :cons ("e m")
+     :operator or}
     {:numb 3
-     :ante [\r \t]
-     :cons [\p \q]}
+     :ante ("r t")
+     :cons ("p q")
+     :operator or}
     {:numb 4
-     :ante [\d \j \e \m \k \i]
-     :cons [\a \c]}
+     :ante ("d j" "e m" "k i")
+     :cons ("a c")
+     :operator or}
     {:numb 5
-     :ante [\p \q]
-     :cons [\n \s]}
+     :ante ("p q")
+     :cons ("n s")
+     :operator or}
     {:numb 6
-     :ante [\u \v]
-     :cons [\k \i]}))
+     :ante ("u v")
+     :cons ("k i")
+     :operator or}))
 
 ; initial working memory
 (def wm
-  '(\f \h \d \j \u \v \r \t))
+  #{"f h"
+    "d j"
+    "u v"
+    "r t"})
