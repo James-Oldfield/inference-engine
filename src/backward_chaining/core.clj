d8efc96d21545130748bf3fe8b2729949088fdab
James Oldfield <hi@james-oldfield.co>
2017-11-21 15:43:24 +0000

    add fn to test if goal is in working memory

--- src/backward_chaining/core.clj
+++ src/backward_chaining/core.clj
@@ -2,6 +2,15 @@
   (:require [backward-chaining.rules :as rules])
   (:gen-class))
 
+; ----------------------
+; Helper functions to operate on above datastructures
+; ----------------------
+
+; return a boolean specifying whether the goal is in the working memory
+(defn fact-in-wm
+  [fact]
+  (contains? rules/wm fact))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
