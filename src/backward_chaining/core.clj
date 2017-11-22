5f1026f9e5cea83b41770eaa33f7b57a50290caa
James Oldfield <hi@james-oldfield.co>
2017-11-22 14:33:52 +0000

    add function to filter rules by consequents

--- src/backward_chaining/core.clj
+++ src/backward_chaining/core.clj
@@ -6,7 +6,13 @@
 ; Helper functions to operate on above datastructures
 ; ----------------------
 
-; return a boolean specifying whether the goal is in the working memory
+;; :return: a lazySeq of rules whose consequents contain the goal symbol
+(defn get-rules-by-cons
+  [goal]
+  (filter #(contains? (get %1 :cons) goal)
+          rules/base))
+
+;; :return: a boolean specifying whether the goal is in the working memory
 (defn fact-in-wm
   [fact]
   (contains? rules/wm fact))
