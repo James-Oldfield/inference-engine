63e28b26b93497252d7bd8066828e50152861bc5
James Oldfield <hi@james-oldfield.co>
2017-11-22 16:51:49 +0000

    print logs about fact's being in WM

--- src/backward_chaining/core.clj
+++ src/backward_chaining/core.clj
@@ -2,9 +2,9 @@
   (:require [backward-chaining.rules :as rules])
   (:gen-class))
 
-; ----------------------
-; Helper functions to operate on above datastructures
-; ----------------------
+;; ----------------------
+;; Helper functions to operate on above datastructures
+;; ----------------------
 
 ;; :return: a lazySeq of rules whose consequents contain the goal symbol
 (defn get-rules-by-cons
@@ -13,7 +13,7 @@
         (filter (fn [rule]
                   (contains? (get rule :cons) goal))
                 rules/base)]
-    (print "-----\n")
+    (print "\n-----\n")
     (print "Matched rules for goal:" goal "-" (map :numb matching-rules))
     (print "\n-----\n")
     matching-rules))
@@ -21,9 +21,18 @@
 ;; :return: a boolean specifying whether the goal is in the working memory
 (defn fact-in-wm?
   [fact]
-  (contains? rules/wm fact))
+  (let [fact-in-wm (contains? rules/wm fact)]
+    (print "Fact" fact "in memory? -" fact-in-wm)
+    fact-in-wm))
+
+(defn match
+  [goal]
+  (or (fact-in-wm? goal)
+      (print "recur here")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Takes a goal and runs it through inference engine"
   [& args]
-  (print (map :ante rules/base)))
+  (let [goal "b g"]
+    (fact-in-wm? goal)
+    (get-rules-by-cons goal)))
