1e9f250bd97393084add82bab875aa91b2d987dd
James Oldfield <hi@james-oldfield.co>
2017-11-23 15:44:31 +0000

    implement main recursive traversal fn

--- src/backward_chaining/core.clj
+++ src/backward_chaining/core.clj
@@ -13,26 +13,41 @@
         (filter (fn [rule]
                   (.contains (get rule :cons) goal))
                 rules/base)]
-    (print "\n-----\n")
-    (print "Matched rules for goal:" goal "-" (map :numb matching-rules))
-    (print "\n-----\n")
+    (print "\n\nMatched rules for goal:" goal "-" (map :numb matching-rules))
     matching-rules))
 
 ;; :return: a boolean specifying whether the goal is in the working memory
 (defn fact-in-wm?
   [fact]
   (let [fact-in-wm (.contains rules/wm fact)]
-    (print "Fact" fact "in memory? -" fact-in-wm)
+    (print "\nFact" fact "in memory? -" fact-in-wm)
     fact-in-wm))
 
-(defn match
+(defn prove
   [goal]
-  (or (fact-in-wm? goal)
-      (print "recur here")))
+  ;; use tail-call recursion
+  (loop [subgoal goal
+         queue '()]
+
+    ;; If current goal is found, recur with rest of goals
+    (if (fact-in-wm? subgoal)
+      (if (empty? queue)
+        (print "\nLast goal proven -" subgoal "=>" goal)
+        (recur (first queue) (rest queue)))
+
+      ;; bind a single flattened vector of antecedents sufficient for current subgoal.
+      ;; i.e. single vec of antecedents of every rule with subgoal as consequent.
+      (let [subgoal-queue
+            (flatten (map :ante (get-rules-by-cons subgoal)))]
+        (print "\nNew subgoals -" subgoal-queue)
+
+        ;; Recur with next subgoal in depth-first manner, if !empty?
+        (if (empty? subgoal-queue)
+          (print "no reminaing subgoals")
+          (recur (first subgoal-queue) (rest subgoal-queue)))))))
 
 (defn -main
   "Takes a goal and runs it through inference engine"
   [& args]
   (let [goal "b g"]
-    (fact-in-wm? goal)
-    (get-rules-by-cons goal)))
+    (prove goal)))
