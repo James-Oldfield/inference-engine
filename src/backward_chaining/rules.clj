a14346eca9db24681c53b381f57960500cdc7914
James Oldfield <hi@james-oldfield.co>
2017-11-29 12:53:53 +0000

    use chars over strings

--- src/backward_chaining/rules.clj
+++ src/backward_chaining/rules.clj
@@ -11,48 +11,48 @@
 (def base
   '(
     ;; Test dead end rules
-    {:numb 100
-     :ante ["x x" "l l"]
-     :cons ["b g"]
-     :operator or}
-    {:numb 101
-     :ante ["y y"]
-     :cons ["x x"]
-     :operator or}
-    {:numb 102
-     :ante ["z z"]
-     :cons ["y y"]
-     :operator or}
+    ; {:numb 100
+    ;  :ante ["x x" "l l"]
+    ;  :cons ["b g"]
+    ;  :operator or}
+    ; {:numb 101
+    ;  :ante ["y y"]
+    ;  :cons ["x x"]
+    ;  :operator or}
+    ; {:numb 102
+    ;  :ante ["z z"]
+    ;  :cons ["y y"]
+    ;  :operator or}
     ;; End Test dead end rules
 
     {:numb 1
-     :ante ["f h" "a c"]
-     :cons ["b g"]
+     :ante [[\f \h] [\a \c] []]
+     :cons [\b \g]
      :operator or}
     {:numb 2
-     :ante ["n s"]
-     :cons ["e m"]
+     :ante [[\n \s] [] []]
+     :cons [\e \m]
      :operator or}
     {:numb 3
-     :ante ["r t"]
-     :cons ["p q"]
+     :ante [[\r \t] [] []]
+     :cons [\p \q]
      :operator or}
     {:numb 4
-     :ante ["d j" "e m" "k i"]
-     :cons ["a c"]
+     :ante [[\d \j] [\e \m] [\k \i]]
+     :cons [\a \c]
      :operator or}
     {:numb 5
-     :ante ["p q"]
-     :cons ["n s"]
+     :ante [[\p \q] [] []]
+     :cons [\n \s]
      :operator or}
     {:numb 6
-     :ante ["u v"]
-     :cons ["k i"]
+     :ante [[\u \v] [] []]
+     :cons [\k \i]
      :operator or}))
 
 ;; initial working memory
 (def wm
-  ["f h"
-    "d j"
-    "u v"
-    "r t"])
+  [[\f \h]
+   [\d \j]
+   [\u \v]
+   [\r \t]])
