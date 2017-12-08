fc5f170ae44faca4b239af11d337d5a9462216cf
James Oldfield <hi@james-oldfield.co>
2017-12-08 18:50:30 +0000

    more tidying

--- src/backward_chaining/core.clj
+++ src/backward_chaining/core.clj
@@ -17,12 +17,12 @@
 ;; :return: a lazySeq of rules whose consequents contain the goal symbol
 ;; N.B. each rule may contain many antecedents (i.e. nested seq)
 (defn match
-  [goal]
+  [goal log?]
   (let [matching-rules
         (filter (fn [rule]
                   (= (get rule :cons) goal))
                 rules/as-ints)]
-    ; (print "\nMatched rules for goal:" (intvec-to-char goal) "-" (map :numb matching-rules))
+    (and log? (print "\nMatched rules for goal:" (intvec-to-char goal) "-" (map :numb matching-rules)))
     matching-rules))
 
 ;; Given matched rules, returns antecedents that need to be next proven
@@ -41,9 +41,9 @@
 
 ;; :return: a boolean specifying whether the goal is in the working memory
 (defn fact-in-wm?
-  [fact memory]
+  [fact memory log?]
   (let [fact-in-wm (.contains memory fact)]
-    ; (print "\nFact" (intvec-to-char fact) "in memory? -" fact-in-wm)
+    (and log? (print "\nFact" (intvec-to-char fact) "in memory? -" fact-in-wm))
     fact-in-wm))
 
 (defn prove
@@ -62,21 +62,27 @@
          backtrack false
          memory rules/wm-as-int]
 
-    (print "\n\nCurrent subgoal -" (intvec-to-char subgoal))
-    (print "\nWorking memory -" (set (intvec-to-char memory)))
-    (print "\nFrontier" (intvec-to-char frontier))
+    ;; LOG PROGRESS
+    (if backtrack
+      (print "\nBacktracking to" (intvec-to-char subgoal) "...\n")
+      (do
+        (print "\n\nCurrent subgoal -" (intvec-to-char subgoal))
+        (print "\nWorking memory -" (set (intvec-to-char memory)))
+        (print "\nFrontier" (intvec-to-char frontier))))
+    ;; END LOG
 
-    (let [rules (match subgoal)               ;; match all rules concerning this subgoal
-          antecedents (match-antes rules)     ;; match the rules to relevant antecedents
-          queue (select antecedents visited)  ;; promote new subgoals we haven't visited
-          true-fact (fact-in-wm? subgoal memory)]
+    (let [rules (match subgoal (not backtrack)) ;; match all rules concerning this subgoal
+          antecedents (match-antes rules)       ;; match the rules to relevant antecedents
+          queue (select antecedents visited)    ;; promote new subgoals we haven't visited
+          true-fact (fact-in-wm? subgoal memory (not backtrack))]
 
-      (print "\nNew queue for expanded leaf node -" (intvec-to-char queue))
+      (if (not backtrack) (print "\nNew queue for expanded leaf node -" (intvec-to-char queue)))
 
+      ;; check for end of branch
       (if (or true-fact (empty? queue))
         (if (= goal subgoal) ;; If we are at root node with no more rules (i.e. empty queue), test for success
-          (if (every? true? (map #(fact-in-wm? % memory) ;; Is every/any antecedent for goal node satisfied?
-                               (select antecedents '())))
+          ;; Is every/any antecedent for goal node satisfied?
+          (if (every? true? (map #(fact-in-wm? % memory true) (select antecedents '())))
             (print "\n\nRequisite antecedents satisfied." (intvec-to-char goal) "is true.")
             (print "\n\nNo more rules found for goal =>" (intvec-to-char goal) "is not true."))
 
@@ -84,15 +90,17 @@
                  []                     ;; Reset frontier
                  (rest prnts)           ;; Remove new subgoal from parents
                  (conj visited subgoal) ;; we've also now visited this `subgoal`
-                 true                      ;; flag that we're backtracking
-                 (if (and backtrack (fact-in-wm? (last visited) memory))
-                   (concat (seq [subgoal]) memory) memory))) ;; if we're backtracking, add current subgoal to wm memory
+                 true                   ;; flag that we're backtracking
+                 ;; Child node being true => this node is true, provding queue is empty
+                 (if (and backtrack (fact-in-wm? (last visited) memory (not backtrack)))
+                   (concat (seq [subgoal]) memory)
+                   memory)))
 
         ;; else carry on picking facts off this branch
-        (recur (first queue)                  ;; take top of queue as new subgoal
-               (rest queue) ;; queue takes priority over frontier, as depth first
-               (conj prnts subgoal)           ;; subgoal is now the most recent parent
-               (conj visited subgoal)         ;; we've also now visisted `subgoal`
+        (recur (first queue)          ;; take top of queue as new subgoal
+               (rest queue)           ;; push rest of queue to the frontier 
+               (conj prnts subgoal)   ;; subgoal is now the most recent parent
+               (conj visited subgoal) ;; we've also now visisted `subgoal`
                false
                memory)))))
 
